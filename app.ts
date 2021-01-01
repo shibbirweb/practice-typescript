33ef66bb9b7839cb4d332451640cdbc7862928a0
Shibbir Ahmed <shibbirweb@gmail.com>
2021-01-01 15:54:52 +0600

    Complete

--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
-console.log("Your code goes here...");
+// Core Type of JavaScript
+/* 
+    1. number - 1,3,5,1.5,1.9
+    2. string - "Hi", 'Hi', `Hi`
+    3. boolean - true, false
+*/
+
+function add(n1: number, n2: number){
+    
+    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error("Incorrect input!!!")
+    }
+    
+    return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.8;
+
+const result = add(number1, number2);
+
+console.log(result);
+
